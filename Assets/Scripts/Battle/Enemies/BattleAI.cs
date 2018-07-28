96c8fe4f86e60e0b938b45ac726c553d9d047296
Jed Chen <jedido101@yahoo.com>
2018-07-28 02:16:26 -0700

    Added slime battle AI

--- Assets/Scripts/Battle/Enemies/BattleAI.cs
+++ Assets/Scripts/Battle/Enemies/BattleAI.cs
@@ -5,14 +5,43 @@ using UnityEngine;
 // In charge of movement and attacks during battle phase
 public abstract class BattleAI : MonoBehaviour {
     private int health;
+    private Rigidbody2D rb2d;
     private Enemy e;
+    protected GameObject smallProj, proj, bigProj;
+    protected Vector2 Velocity
+    {
+        set { rb2d.velocity = value; }
+    }
 
     private void Start()
     {
         health = GetHealth();
+        rb2d = GetComponent<Rigidbody2D>();
+        smallProj = SpriteLibrary.library.SmallProjectile;
     }
 
     protected abstract int GetHealth();
+
+    // 0 is right, 1 is up, 2 is left, else is down
+    protected bool CanMove(int direction)
+    {
+        Vector2 dir;
+        switch (direction)
+        {
+            case 0: dir = new Vector2(1, 0); break;
+            case 1: dir = new Vector2(0, 1); break;
+            case 2: dir = new Vector2(-1, 0); break;
+            default: dir = new Vector2(0, -1); break;
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 0.5f);
+        return hit.collider == null || !hit.collider.tag.Equals("Wall");
+    }
+
+    protected bool CanMove(Vector2 dir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 0.5f);
+        return hit.collider == null || !hit.collider.tag.Equals("Wall");
+    }
 
     public void SetEnemy(Enemy enemy)
     {
--- Assets/Scripts/Battle/Enemies/BattleSlime.cs
+++ Assets/Scripts/Battle/Enemies/BattleSlime.cs
@@ -1,7 +1,16 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+// Generic Slime AI
+// Movement: chooses a cardinal direction towards the player and moves there for a set amount of time, 
+// then chooses a new direction. If it hits a wall, chooses a new direction immediately.
+
 public class BattleSlime : BattleAI {
+    private static readonly float speed = 2f;
+    private static readonly float walkCooldown = 0.5f;
+    private static readonly float shotCooldown = 1f;
+    private float walkTimer, shotTimer;
+    private Vector2 direction;
 
     protected override int GetHealth()
     {
@@ -10,6 +19,58 @@ public class BattleSlime : BattleAI {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Attack
+        if (shotTimer < Time.time)
+        {
+            shotTimer = Time.time + shotCooldown;
+            EnemyProjectile up = Instantiate(smallProj, transform.position, Quaternion.identity).GetComponent<EnemyProjectile>();
+            EnemyProjectile down = Instantiate(smallProj, transform.position, Quaternion.identity).GetComponent<EnemyProjectile>();
+            EnemyProjectile left = Instantiate(smallProj, transform.position, Quaternion.identity).GetComponent<EnemyProjectile>();
+            EnemyProjectile right = Instantiate(smallProj, transform.position, Quaternion.identity).GetComponent<EnemyProjectile>();
+            up.InitialVelocity = new Vector2(0, 5f);
+            down.InitialVelocity = new Vector2(0, -5f);
+            left.InitialVelocity = new Vector2(-5f, 0);
+            right.InitialVelocity = new Vector2(5f, 0);
+        }
+
+        // Movement
+        Velocity = direction;
+        if (walkTimer < Time.time)
+        {
+            walkTimer = Time.time + walkCooldown;
+            Vector2 dir = PlayerManager.player.battleAlien.transform.localPosition - transform.localPosition;
+            if (CanMove(dir))
+            {
+                SetDirection(dir);
+            } else
+            {
+                float x = Mathf.Abs(dir.x);
+                Vector2 dirX = new Vector2(dir.x, 0);
+                float y = Mathf.Abs(dir.y);
+                Vector2 dirY = new Vector2(0, dir.y);
+                SetDirection(CanMove(dirX) ? dirX : CanMove(dirY) ? dirY : CanMove(-dirX) ? -dirX : -dirY);
+            }
+        }
+    }
+
+    private void SetDirection(Vector2 dir)
+    {
+        Velocity = direction = dir.normalized * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Wall"))
+        {
+            walkTimer = 0;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Wall"))
+        {
+            walkTimer = 0;
+        }
+    }
 }
